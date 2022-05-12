ce65453c36a9909fdbb95673bdd8a2c9228af2c6
Anou <anousone.mounivongs@gmail.com>
2022-05-12 17:02:40 +0200

    mise à jour mémo

--- public/script/script.js
+++ public/script/script.js
@@ -4,16 +4,16 @@ let object = document.getElementById('object')
 let memoText = document.getElementById('memoText')
 
 // Variable pour compter nos memos
-let countMemo = 1 
+let countMemo = 6
 
 // fonction qui permets de nettoyer les inputs
-function cleanForm(){
+function cleanForm() {
     object.value = ""
     memoText.value = ""
 }
 
 // fonction permettant de créer un memo
-function createMemo() {
+function createNewMemo() {
 
     // j'utilise innerHTML avec le += pour insérer mes mémos
     allMemo.innerHTML += `
@@ -30,9 +30,58 @@ function createMemo() {
 }
 
 // fonction pour effacer un élément
-function deleteMemo(element){
+function deleteMemo(element) {
     let elementToDelete = document.getElementById(element)
     elementToDelete.remove()
 }
 
+
+function createMemo() {
+    if (object.value != '' && memoText.value != '') {
+        createNewMemo()
+    } else {
+        window.alert('ATTENTION TOUS LES CHAMPS DOIVENT ETRE REMPLIS')
+    }
+}
+
+// ------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------
+
+let myArray = [
+    {
+        id: 'memo-1',
+        object: 'Kebab',
+        memoText: 'Stella : Complet sans tomate / Samuraï'
+    },
+    {
+        id: 'memo-2',
+        object: 'Kebab',
+        memoText: 'Mickael : Complet / Algérienne'
+    },
+    {
+        id: 'memo-3',
+        object: 'Kebab',
+        memoText: 'Sophie : Complet sans sauce'
+    },
+    {
+        id: 'memo-4',
+        object: 'Kebab',
+        memoText: 'Valentin : Complet / Mayo'
+    },
+    {
+        id: 'memo-5',
+        object: 'Kebab',
+        memoText: 'Alexandre : Complet sans tomate / Andalouse'
+    }
+]
+
+myArray.forEach(element => {
+    allMemo.innerHTML += `
+    <div id="${element.id}" class="note shadow pt-1 pb-2 ps-4 pe-2 rounded">
+        <div class="close"><i class="bi bi-x-square-fill" onclick="deleteMemo('${element.id}')"></i></div>
+        <h2 class="fs-4 m-0">${element.object}</h2>
+        <p>${element.memoText}</p>
+    </div>
+`
+})
 
